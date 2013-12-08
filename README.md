777f5d9c7e6b5ab0c5c94fd6644f532b7864d242
petehunt <floydophone@gmail.com>
2013-12-08 14:15:59 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-raf-batching
+
+A batching strategy for React that's more like a game. This may make your apps more performant but harder to test.
---
+++ ReactRAFBatchingStrategy.js
@@ -0,0 +1,44 @@
+/**
+ * Copyright 2013 Facebook, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * @providesModule ReactRAFBatchingStrategy
+ */
+
+"use strict";
+
+var ReactUpdates = require('react/lib/ReactUpdates');
+
+var requestAnimationFrame = require('./requestAnimationFrame');
+
+function tick() {
+  ReactUpdates.flushBatchedUpdates();
+  requestAnimationFrame(tick);
+}
+
+var ReactRAFBatchingStrategy = {
+  isBatchingUpdates: true,
+
+  /**
+   * Call the provided function in a context within which calls to `setState`
+   * and friends are batched such that components aren't updated unnecessarily.
+   */
+  batchedUpdates: function(callback, param) {
+    callback(param);
+  }
+};
+
+requestAnimationFrame(tick);
+
+module.exports = ReactRAFBatchingStrategy;
