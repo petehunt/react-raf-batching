aff118fcb57081025e129fe4a2e875d5681227ed
Aleksandar Ružičić <aleksandar@ruzicic.info>
2015-04-15 18:50:56 +0200

    Pass all parameters provided to batchedUpdates method to the callback
    This fixes 'Target container is not valid' invariant violation that
    occurs in recent React versions (more than one parameter, other than
    callback, is passed in batchedUpdates call).

--- ReactRAFBatchingStrategy.js
+++ ReactRAFBatchingStrategy.js
@@ -33,8 +33,8 @@ var ReactRAFBatchingStrategy = {
    * Call the provided function in a context within which calls to `setState`
    * and friends are batched such that components aren't updated unnecessarily.
    */
-  batchedUpdates: function(callback, param) {
-    callback(param);
+  batchedUpdates: function(callback) {
+    callback.apply(void 0, Array.prototype.slice.call(arguments, 1));
   }
 };
 
